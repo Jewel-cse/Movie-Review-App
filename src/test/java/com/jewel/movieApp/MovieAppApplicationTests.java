c4a8832bbd2df037c62be8f4eddc57b2a787cff1
Jewel-cse <md.jewel.cse@gmail.com>
2024-03-06 09:16:49 +0600

    initial commit

---
+++ src/test/java/com/jewel/movieApp/MovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewel.movieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
